f6b35f2c140eeed6c4dd6f407759990f1730ddfe
root <jeffrey_choy@yeah.net>
2019-12-09 09:11:26 +0000

    123

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,31 +1,44 @@
-import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
-import { AtButton } from 'taro-ui'
+import Taro from '@tarojs/taro'
+import { AtForm, AtInput, AtButton } from 'taro-ui'
 import './index.scss'
 
-export default class Index extends Component {
-
-  config = {
-    navigationBarTitleText: '首页'
+export default class Index extends Taro.Component {
+  constructor () {
+    super(...arguments)
+    this.state = {
+      value: ''
+    }
   }
-
-  componentWillMount () { }
-
-  componentDidMount () { }
-
-  componentWillUnmount () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
+  handleChange (value) {
+    this.setState({
+      value
+    })
+  }
+  onSubmit (event) {
+    console.log(this.state.value)
+  }
+  onReset (event) {
+    this.setState({
+      value: '',
+    })
+  }
   render () {
     return (
-      <View className='index'>
-        <Text>Hello world!</Text>
-        <AtButton type='primary'>按钮文案</AtButton>
-      </View>  
-          
+      <AtForm
+        onSubmit={this.onSubmit.bind(this)}
+        onReset={this.onReset.bind(this)}
+      >
+        <AtInput 
+          name='value' 
+          title='文本' 
+          type='text' 
+          placeholder='单行文本' 
+          value={this.state.value} 
+          onChange={this.handleChange.bind(this, 'value')} 
+        />
+        <AtButton formType='submit'>提交</AtButton>
+        <AtButton formType='reset'>重置</AtButton>
+      </AtForm>
     )
   }
 }
